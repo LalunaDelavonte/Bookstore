d74e2ddc12613dd92a2cf170616fa6dd8e9d8b1b
WidyaPuAs <edithcasean@gmail.com>
2023-06-07 18:38:32 +0700

    instalasi

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/home";
+
+function App() {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <h1></h1>
+  );
+}
+
+export default App;
